728dd5d90268cd0a80db3eb2f928cb68afd884b5
Lewis Xingwen <shuaicode@gmail.com>
2024-07-29 00:19:11 +0800

    fix

--- config/routes.ts
+++ config/routes.ts
@@ -18,4 +18,36 @@ export default [
       path: '/table',
       component: './Table',
     },
+
+    {
+      name: '系统管理',
+      path: '/system',
+      icon: 'setting',
+      routes: [
+        {
+          name: '菜单管理',
+          path: '/system/menu',
+          component: './Sys/Menu',
+        },
+        {
+          name: '登录日志',
+          path: '/system/loginlog',
+          component: './Sys/LoginLog',
+        },
+        {
+          name: '操作日志',
+          path: '/system/oplog',
+          component: './Sys/OpLog',
+        },
+        {
+          name: 'API管理',
+          path: '/system/api',
+          component: './Sys/Api',
+        },
+      ],
+    },
+    {
+      component: '404',
+      path: '/*',
+    },
   ];
